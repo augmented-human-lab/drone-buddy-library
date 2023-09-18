587ad6f4b30511e1b66430d417934f82d4a2abd5
Malsha de Zoysa <malshadezoysa92@gmail.com>
2023-09-18 15:30:40 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -5,7 +5,7 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-import sphinx_rtd_theme
+import alabaster
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
