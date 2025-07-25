a525b51d8d0d5ffe3f3f1c4326143118354aae7c
Zen <zhengrong_chia@yahoo.com>
2025-07-25 10:20:15 +0800

    Updates: video still not working

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ something...
 import sys
 import os
 import time
-from djitellopy import tello
+from djitellopy import Tello
 
 os.environ['QT_QPA_PLATFORM'] = 'xcb'
 import cv2
@@ -89,7 +89,29 @@ if __name__ == "__main__":
     #     print(f"An error occurred: {e}")
     #     video.stop()
     #     cv2.destroyAllWindows()
-    main()
+    # main()
+
+
+    # def tello_get_frame(tello: Tello, w=360, h=240):
+    #     frame = tello.get_frame_read().frame
+    #     if frame is not None:
+    #         return cv2.resize(frame, (w, h))
+    #     return None
+    
+    # tello = Tello()
+    # tello.connect(wait_for_state=False)
+    # tello.streamon()
+    # # time.sleep(2)  # Allow time for the video stream to start
+    # print("Tello connected successfully")
+    # while True:
+    #     processed_frame = tello_get_frame(tello, w=360, h=240)
+    #     cv2.imshow('Tello Video', processed_frame)
+    #     if cv2.waitKey(1) & 0xFF == ord('q'):
+    #         break
+    # cv2.destroyAllWindows()
+    # tello.streamoff()
+    # tello.end()
+
 
     """
     (venv) zen@soccf-isa3-001:~/drone-buddy-library$ sudo nmap -sU 192.168.10.1 -p 8889,8890,11111
@@ -105,3 +127,54 @@ PORT      STATE         SERVICE
 
 Nmap done: 1 IP address (1 host up) scanned in 1.45 seconds
     """
+
+"""
+(venv) zen@soccf-isa3-001:~/drone-buddy-library$ python3 main.py
+[INFO] tello.py - 129 - Tello instance was initialized. Host: '192.168.10.1'. Port: '8889'.
+INFO:djitellopy:Tello instance was initialized. Host: '192.168.10.1'. Port: '8889'.
+[INFO] tello.py - 438 - Send command: 'command'
+INFO:djitellopy:Send command: 'command'
+[ERROR] tello.py - 458 - 'utf-8' codec can't decode byte 0xcc in position 0: invalid continuation byte
+ERROR:djitellopy:'utf-8' codec can't decode byte 0xcc in position 0: invalid continuation byte
+[INFO] tello.py - 438 - Send command: 'command'
+INFO:djitellopy:Send command: 'command'
+[INFO] tello.py - 462 - Response command: 'ok'
+INFO:djitellopy:Response command: 'ok'
+[INFO] tello.py - 438 - Send command: 'streamon'
+INFO:djitellopy:Send command: 'streamon'
+[INFO] tello.py - 462 - Response streamon: 'ok'
+INFO:djitellopy:Response streamon: 'ok'
+Tello connected successfully
+Traceback (most recent call last):
+  File "/home/zen/drone-buddy-library/venv/lib/python3.12/site-packages/djitellopy/tello.py", line 1049, in __init__
+    self.container = av.open(self.address, timeout=(Tello.FRAME_GRAB_TIMEOUT, None))
+                     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  File "av/container/core.pyx", line 420, in av.container.core.open
+  File "av/container/core.pyx", line 266, in av.container.core.Container.__cinit__
+  File "av/container/core.pyx", line 286, in av.container.core.Container.err_check
+  File "av/error.pyx", line 328, in av.error.err_check
+av.error.ExitError: [Errno 1414092869] Immediate exit requested: 'udp://@0.0.0.0:11111'
+
+During handling of the above exception, another exception occurred:
+
+Traceback (most recent call last):
+  File "/home/zen/drone-buddy-library/main.py", line 105, in <module>
+    processed_frame = tello_get_frame(tello, w=360, h=240)
+                      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  File "/home/zen/drone-buddy-library/main.py", line 94, in tello_get_frame
+    frame = tello.get_frame_read().frame
+            ^^^^^^^^^^^^^^^^^^^^^^
+  File "/home/zen/drone-buddy-library/venv/lib/python3.12/site-packages/djitellopy/enforce_types.py", line 54, in wrapper
+    return func(*args, **kwargs)
+           ^^^^^^^^^^^^^^^^^^^^^
+  File "/home/zen/drone-buddy-library/venv/lib/python3.12/site-packages/djitellopy/tello.py", line 421, in get_frame_read
+    self.background_frame_read = BackgroundFrameRead(self, address, with_queue, max_queue_len)
+                                 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  File "/home/zen/drone-buddy-library/venv/lib/python3.12/site-packages/djitellopy/tello.py", line 1051, in __init__
+    raise TelloException('Failed to grab video frames from video stream')
+djitellopy.tello.TelloException: Failed to grab video frames from video stream
+[INFO] tello.py - 438 - Send command: 'streamoff'
+INFO:djitellopy:Send command: 'streamoff'
+[INFO] tello.py - 462 - Response streamoff: 'ok'
+INFO:djitellopy:Response streamoff: 'ok'
+"""
